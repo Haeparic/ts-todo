1999230d1cc34ed6ad7a510004479cc24a241c41
Haeparic <v567v202@gmail.com>
2023-03-17 12:20:13 +0900

    react-thunk 적용

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,37 +1,36 @@
 // store 관련
 import { useSelector, useDispatch } from "react-redux";
-import type { RootState } from "./store/store";
+import type { AppDispatch, RootState } from "./store/store";
 import {
   fbLoginState,
   fbJoinState,
   fbLogoutState,
   fbDeleteUserState,
 } from "./store/userSlice";
-
-// firebase 관련
-import { fireDB, auth } from "./firebase";
 import {
-  collection,
-  deleteDoc,
-  doc,
-  getDocs,
-  query,
-  setDoc,
-  updateDoc,
-} from "firebase/firestore";
+  initTodoState,
+  addTodoState,
+  updateTodoState,
+  deleteTodoState,
+  sortTodoState,
+  clearTodoState,
+  addTodoFB,
+  deleteTodoFB,
+  updateTodoFB,
+  clearTodoFB,
+  getTodoFB,
+} from "./store/todoSlice";
+// firebase 관련
+import { auth } from "./firebase";
 
-import { useEffect, useState } from "react";
-// 객체복사 라이브러리
-import produce from "immer";
+import { useEffect } from "react";
 import App from "./App";
-import moment from "moment";
 import {
   createUserWithEmailAndPassword,
   deleteUser,
   signInWithEmailAndPassword,
   User,
 } from "firebase/auth";
-
 export type TodoType = {
   uid: string;
   title: string;
@@ -39,16 +38,6 @@ export type TodoType = {
   done: boolean;
   sticker: string;
   date: string;
-};
-
-// 상태를 변경하는 함수를 묶어서 타입으로 정의해 볼까?
-// 꼭 타입으로 정의해서 진행하지 않아도 됨, 즉, 처음부터 최적화를 하는것은 좋지 않음
-// 나 혼자 관리하는 개발을 주도 : 타입정의 하는 것이 좋다
-// 타인과 개발한다 : 고려해 봐야 한다
-// 단점 : 타인이 Type 에 대한 구성을 파악하고 학습할 시간이 필요
-// 장점 : 오류가 줄어듦(오타, 데이터 값의 종류), 안정성이 높아짐
-export type StatesType = {
-  todoList: Array<TodoType>;
 };
 
 export type CallBacksType = {
@@ -65,58 +54,29 @@ export type CallBacksType = {
   sortTodo: (sortType: string) => void;
   clearTodo?: () => void;
 };
+export type StatesType = {
+  todoList: Array<TodoType>;
+};
 
-// 로그인 및 회원가입 타입 정의
+// 로그인 및 회원가입 타입정의
 export type CallBacksFireBaseType = {
-  fbLogin: (email: string, passwoard: string) => void;
-  fbJoin: (email: string, passwoard: string) => void;
+  fbLogin: (email: string, password: string) => void;
+  fbJoin: (email: string, password: string) => void;
   fbLogout: () => void;
   fbDeleteUser: () => void;
 };
 
 const AppContainer = () => {
-  // store 관련
-  const dispatch = useDispatch();
+  // store 코드
+  const dispatch = useDispatch<AppDispatch>();
   const user = useSelector((state: RootState) => state.user);
+  const todo = useSelector((state: RootState) => state.todo);
 
-  // 상태데이터
-  let initData: Array<TodoType> = [];
-
-  // 로컬스토리지 이름
-  // const localStorageName = "tstodo";
-
-  // firebase Storage 이름
-  const firebaseStorageName = "tsmemo";
-  // 컬렉션(DataBase 단위 : MongoDB 참조) 불러오기
-  const memoCollectionRef = collection(fireDB, firebaseStorageName);
-
-  // 로컬스토리지 활용 -> firebase 로 변경
+  // firebase 전체 데이터 로드
   const getLocalData = async () => {
-    // const data = localStorage.getItem(localStorageName);
-    const q = await query(memoCollectionRef);
-    const data = await getDocs(q);
-    // console.log("firebase collection data : ", data);
-
-    if (data !== null) {
-      // initData = JSON.parse(data);
-      // 모든 데이터 가져와서 뜯기
-      // [ {}, {}, {}, ... ]
-      const firebaseData = data.docs.map((doc) => ({
-        ...doc.data(),
-      }));
-      // firebaseData = [ {}, {}, {}, ... ]
-      const initData = firebaseData.map((item) => {
-        // firebase 에서 가져온 데이터를 TypeScript 에서 우리가 만든 Type 으로 형변환하기
-        return item as TodoType;
-      });
-      // setTodoList(Array<TodoType>) 형을 원했다
-      setTodoList(initData);
-    }
+    dispatch(getTodoFB());
   };
-
-  // 화면의 내용을 갱신해 주기 위해서 state Hook 사용
-  const [todoList, setTodoList] = useState<Array<TodoType>>(initData);
-  // 추가기능
+  // fb 추가기능
   const addTodo = async (
     uid: string,
     title: string,
@@ -125,136 +85,56 @@ const AppContainer = () => {
     sticker: string,
     date: string
   ) => {
-    // firebase 에 쓰기
-    try {
-      const res = await setDoc(doc(fireDB, firebaseStorageName, uid), {
+    dispatch(
+      addTodoFB({
         uid: uid,
         title: title,
         body: body,
         date: date,
         sticker: sticker,
         done: false,
-      });
-      console.log(res); // res는 undefined입니다.
-    } catch (e) {
-      console.log(e);
-    }
-
-    // 새로운 todoType 생성
-    // 기존 todoList state 를 복사하고, 추가 todoList 를 합쳐주고
-    // todoList state 를 업데이트한다.
-    // 이때, immutable 을 유지한다.
-    // 필수 라이브러리로 Immer 가 있다.
-    // Immer 는 객체의 불변성을 유지하는 것으로 업무에서 필수로 활용한다.
-    // 즉, {...todoList, newTodo} 를 대신한다.
-    let newTodoList = produce(todoList, (draft) => {
-      draft.push({
+      })
+    );
+    dispatch(
+      addTodoState({
         uid: uid,
         title: title,
         body: body,
         date: date,
         sticker: sticker,
         done: false,
-      });
-    });
-    // state 업데이트 : 화면 갱신
-    setTodoList(newTodoList);
-
-    // localStorage.setItem(localStorageName, JSON.stringify(newTodoList));
+      })
+    );
   };
-  // 수정기능
+  // fb 수정기능
   const updateTodo = async (todo: TodoType) => {
-    // 원하는 데이터 가져옴
-    const userDoc = doc(fireDB, firebaseStorageName, todo.uid);
-    try {
-      const res = await updateDoc(userDoc, {
-        title: todo.title,
-        body: todo.body,
-        sticker: todo.sticker,
-        done: todo.done,
-        date: moment(todo.date).format("YYYY-MM-DD"),
-      });
-      console.log(res); // res는 undefined
-    } catch (e) {
-      // console.log(e);
-    } finally {
-      // console.log("end");
-    }
-
-    // console.log("갱신될 내용 : ", todo);
-    // 1. 먼저 uid 를 비교해서 배열의 순서에 맞는 1개를 찾는다.
-    const index = todoList.findIndex((item) => item.uid === todo.uid);
-    // console.log("갱신될 index : ", index);
-    // 2. 해당하는 uid 의 내용을 갱신한다.
-    const newTodoList = produce(todoList, (draft) => {
-      draft[index] = {
-        ...draft[index],
-        title: todo.title,
-        body: todo.body,
-        date: moment(todo.date).format("YYYY-MM-DD"),
-        done: todo.done,
-        sticker: todo.sticker,
-      };
-    });
-    // 3. state를 업데이트한다.
-    setTodoList(newTodoList);
-
-    // localStorage.setItem(localStorageName, JSON.stringify(newTodoList));
+    // 서버 연동으로 비동기로 자료를 업로드
+    dispatch(updateTodoFB(todo));
+    // 동기로 즉시 state 업데이트
+    dispatch(updateTodoState(todo));
   };
-
-  // 삭제기능
+  // fb 삭제기능
   const deleteTodo = async (todo: TodoType) => {
-    // firebase 데이터 1개 삭제
-    const userDoc = doc(fireDB, firebaseStorageName, todo.uid);
-    try {
-      const res = await deleteDoc(userDoc);
-      console.log(res); // res는 undefined
-    } catch (e) {
-      console.log(e);
-    } finally {
-      console.log("end");
-    }
-
-    let index = todoList.findIndex((item) => todo.uid === item.uid);
-    // state 의 목록을 삭제 후 갱신한다. 불변성 라이브러리 (immer) 활용
-    // let newTodoList = produce(대상, (draft)=>{})
-    let newTodoList = produce(todoList, (draft) => {
-      // index 의 순서로부터 1개를 제거하고 나머지 배열을 리턴한다.
-      // 즉, 원본을 복사해서 새로운 배열을 만들고 그 중에 1개를 제거한 후
-      // 새로운 배열을 리턴하여 state 를 업데이트 한다.
-      draft.splice(index, 1);
-    });
-    setTodoList(newTodoList);
-
-    // localStorage.setItem(localStorageName, JSON.stringify(newTodoList));
+    // 서버 연동으로 비동기로 자료를 업로드
+    // 서버 작업
+    dispatch(deleteTodoFB(todo));
+    // 동기로 즉시 state 업데이트
+    // 즉시 store 의 state 갱신
+    dispatch(deleteTodoState(todo));
   };
   // 전체 목록 삭제
   const clearTodo = () => {
-    setTodoList([]);
-
-    todoList.forEach(async (element) => {
-      // firebase 데이터 1개 삭제
-      const userDoc = doc(fireDB, firebaseStorageName, element.uid);
-      try {
-        const res = await deleteDoc(userDoc);
-        // console.log(res); // res는 undefined
-      } catch (e) {
-        console.log(e);
-      } finally {
-        console.log("end");
-      }
+    // 동기로 state 전체 자료 삭제 후 즉시 화면 갱신
+    dispatch(clearTodoState());
+    // 서버로 자료를 하나씩 보내서 처리
+    todo.todoList.forEach(async (element) => {
+      await dispatch(clearTodoFB(element));
     });
-
-    // localStorage.removeItem(localStorageName);
   };
-  // 정렬기능
-  const sortTodo = (sortType: string) => {};
-
-  // 데이터 목록의 타입
-  const states: StatesType = {
-    todoList,
+  // fb 정렬기능
+  const sortTodo = (sortType: string) => {
+    dispatch(sortTodoState(sortType));
   };
-
   // state 관리기능타입
   const callBacks: CallBacksType = {
     addTodo,
@@ -264,8 +144,8 @@ const AppContainer = () => {
     clearTodo,
   };
 
-  // 현재 사용자가 로그인 된 상태인지 아닌지 구별
-  // const [userLogin, setUserLogin] = useState(false);
+  // 데이터목록의 타입
+  const states: StatesType = { todoList: todo.todoList };
 
   // 사용자 로그인 기능
   const fbLogin = (email: string, password: string) => {
@@ -274,8 +154,9 @@ const AppContainer = () => {
         // Signed in
         const user = userCredential.user;
         console.log(user);
-        // setUserLogin(true);
+
         dispatch(fbLoginState({ email, password }));
+        // setUserLogin(true);
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -284,7 +165,6 @@ const AppContainer = () => {
         console.log("errorMessage : ", errorMessage);
       });
   };
-
   // 사용자 가입
   const fbJoin = (email: string, password: string) => {
     createUserWithEmailAndPassword(auth, email, password)
@@ -292,7 +172,9 @@ const AppContainer = () => {
         // Signed in
         const user = userCredential.user;
         console.log(user);
+        // 생각을 더 해보자 ????
         dispatch(fbJoinState());
+        // setUserLogin(true);
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -301,21 +183,20 @@ const AppContainer = () => {
         console.log("errorMessage : ", errorMessage);
       });
   };
-
   // 사용자 로그아웃
   const fbLogout = () => {
     auth.signOut();
-    // setUserLogin(false);
-    dispatch(fbLogoutState());
-  };
 
+    dispatch(fbLogoutState());
+    // setUserLogin(false);
+  };
   // 회원탈퇴
   const fbDeleteUser = async () => {
     await deleteUser(auth.currentUser as User)
       .then(() => {
         // User deleted.
-        // setUserLogin(false);
         dispatch(fbDeleteUserState());
+        // setUserLogin(false);
       })
       .catch((error) => {
         // An error ocurred
